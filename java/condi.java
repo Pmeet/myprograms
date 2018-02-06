6a00f73c84dfd9842b018e2f357af88d79969690
Pmeet <parekhmeet97@gmail.com>
2018-02-06 21:03:26 +0530

    Another lesson in java for condition

---
+++ java/condi.java
@@ -0,0 +1,13 @@
+//This program is for getting familiar with conditions in java
+public class condi{
+  public static void main(String[] args){
+    int[] a={2,5};
+    if(a[0]>a[1]) {
+      System.out.println("F is ^ than S");
+    } else if(a[0]<a[1]) {
+      System.out.println("S is ^ than F");
+    } else {
+      System.out.println("S = F");
+    }
+  }
+}
---
+++ java/highlow.java
@@ -0,0 +1,5 @@
+public class highlow{
+  public static void main(String[] args){
+    
+  }
+}
