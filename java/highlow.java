f9dc8052453e4a6f0af430d1190e1b42e51bdc9b
Pmeet <parekhmeet97@gmail.com>
2018-02-07 20:22:56 +0530

    Testing HighLow

--- java/highlow.java
+++ java/highlow.java
@@ -1,5 +1,11 @@
 public class highlow{
   public static void main(String[] args){
-    
+    int a=10;
+    int b=12;
+    if(a<b){
+      System.out.println("A is less");
+    }else{
+      System.out.println("B is less");
+    }
   }
 }
