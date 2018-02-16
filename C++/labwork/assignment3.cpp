7e07b69286b2c0c24a7ec4b8f92f0213c260829e
Pmeet <parekhmeet97@gmail.com>
2018-02-16 09:13:15 +0530

    friend function program in ass-3

--- C++/labwork/assignment3.cpp
+++ C++/labwork/assignment3.cpp
@@ -71,10 +71,66 @@ void Student::putData(){
     }
 
 }
+
+// Program 2 End
+// Program 6
+class DB;
+
+class DM{
+  float meter,ans;
+  float cm;
+public:
+  friend DM add(DM m1,DB f1);
+  void get_data();
+  void print_data();
+};
+
+class DB{
+  float f;
+  float in;
+public:
+  friend DM add(DM m1,DB f1);
+  void get_Data();
+};
+
+void DM::get_data(){
+  cout<<"\nEnter values of DM object"<<end;
+  cout<<"\nm=";cin>>m;
+  cout<<"\ncm=";cin>>cm;
+}
+void DM::print_data(){
+  cout<<"\nThe result of addition is "<<result1;
+}
+void DB::get_Data(){
+  cout<<"\nEnter values of DB object"<<end;
+  cout<<"\nf=";cin>>f;
+  cout<<"\ninch=";cin>>in;
+}
+DM add(DM m1,DB f1){
+  DM result;
+  result.ans=m1.m+f1.f*(0.3048);
+  result.ans=m1.cm+f1.in*(2.54);
+  return result;
+}
+
+
+// Program 6 end
+
+
+// Program 7
+class m1{
+
+};
+class m2{
+
+};
+// Program 7 End
+
+
 int main(){
 
   int a,i;
-  cout<<"\n1. Class Employee\n2.Class Student\nSelect the program number : ";
+  cout<<"\n1. Class Employee\n2.Class Student\n6.Class DM and DB\nSelect the program number : ";
   cin>>a;
   switch(a){
     case 1:
@@ -89,8 +145,14 @@ int main(){
       }
       break;
   case 2:
-
-
+      break;
+  case 6:
+      DM m1,result;
+      DB f1;
+      m1.get_data();
+      f1.get_Data();
+      result=add(m1,f1);
+      result.print_data();
   };
 
 }
