c696b7d2e2e5b75a54b6f597c13f01b8469eab8b
root <root@XtroTop.localdomain>
2018-02-19 10:45:03 +0530

    Finished with classes and objects

--- java/classAndObject.java
+++ java/classAndObject.java
@@ -1,5 +1,16 @@
+// Intro to classes and objects
+
 public class classAndObject{
   public static void main(String[] args){
-    
+    class Number{
+      int n;
+      public boolean isPos(){
+        if(n>= 0) return true;
+        else return false;
+      }
+    }
+    Number n1 = new Number();
+    n1.n=-25;
+    System.out.println(n1.isPos());
   }
 }
